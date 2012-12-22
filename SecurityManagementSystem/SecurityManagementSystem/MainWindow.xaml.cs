50a4955ca0c34991a11629d38a0b82429a4d09b1
chandra shekhar sengupta <sayan801@gmail.com>
2012-12-22 20:18:30 +0530

    Adding Code

---
+++ SecurityManagementSystem/SecurityManagementSystem/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecurityManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void visitorBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void reportBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void permBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void regBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void vidBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
